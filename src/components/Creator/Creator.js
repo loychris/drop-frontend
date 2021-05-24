93011cb8f31ec131c3ac220d896e78438ac640e9
Chris Loy <cloy202@gmail.com>
2021-05-24 19:40:07 +0200

    Added all State to Creator Component

--- src/components/Creator/Creator.js
+++ src/components/Creator/Creator.js
@@ -9,12 +9,34 @@ class Creator extends Component {
 
   state = {
     selectedId: '5',
-    editingId: '', 
+    editingId: '5', 
+
+    elements: [
+      {
+        type: 'text', 
+        elementId: '5',
+        height: 200,
+        width: 200, 
+        posX: 200,
+        posY: 200,
+        text: 'Text Element 1',
+      },
+      {
+        type: 'text', 
+        elementId: '6',
+        height: 200,
+        width: 200, 
+        posX: 600,
+        posY: 600,
+        text: 'Text Element 2',
+      }
+    ]
   }
 
   select = (e, elementId) => {
     e.stopPropagation();
     e.preventDefault();
+    console.log('SELECTING', elementId);
     this.setState({
       selectedId: elementId, 
       editingId: ''
@@ -22,7 +44,6 @@ class Creator extends Component {
   }
 
   selectAndEdit = (e, elementId) => {
-    console.log('doubleClick')
     e.stopPropagation();
     e.preventDefault();
     this.setState({
@@ -32,13 +53,211 @@ class Creator extends Component {
   }
 
   unSelect = (e) => {
-    console.log('unselect')
     if(this.state.editingId !== 0){
       this.setState({editingId: ''});
     } else {
       this.setState({selectedId: ''});
     }
+  }
+
+  ////////////////////////////////////////////////////
+
+  rectangleMouseDown = (e, elementId) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('rectangleMouseDown', elementId);
+    const element = this.state.elements.find(e => e.elementId === elementId);
+    const prevX = element.posX - e.clientX;
+    const prevY = element.posY - e.clientY;
+
+    const mousemove = (e) => {
+      let elementsNew = this.state.elements.filter(e => e.elementId !== elementId);
+      const newX = prevX + e.clientX;
+      const newY = prevY + e.clientY;
+      this.setState({
+        selectedId: elementId, 
+        elements: [
+          ...elementsNew,
+          {
+            ...element,
+            posX: newX,
+            posY: newY,
+          }
+        ]
+      });
+    } 
+
+    const mouseup = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      console.log('rectangleMouseup');
+      window.removeEventListener('mousemove', mousemove);
+      window.removeEventListener('mouseup', mouseup);
+    }
+      window.addEventListener('mousemove', mousemove);
+      window.addEventListener('mouseup', mouseup);
+    this.setState({selectedId: elementId})
+  }
+
+  onTextInput = (e, elementId) => {
+    let elementsNew = this.state.elements.filter(e => e.elementId !== elementId);
+    const element = this.state.elements.find(e => e.elementId === elementId);
+    const elem = document.getElementById(`${elementId}-input`);
+
+    this.setState({
+      elements: [
+        ...elementsNew,
+        {
+          ...element,
+          height: elem.offsetHeight,
+          text: e.target.innerHTML
+        }
+      ]
+    });
+  }
+
+
+
+  adjustTextElementHeight = (elementId) => {
+
+    console.log('adjustTextElementHeight', elementId)
+
+    let elementsNew = this.state.elements.filter(e => e.elementId !== elementId);
+    const element = this.state.elements.find(e => e.elementId === elementId);
+
+    const elem = document.getElementById(`${elementId}-input`);
+    elem.innerHTML = element.text;
+    console.log(elem.offsetHeight, element.height) 
+    console.log(elementsNew); 
+    this.setState({
+      elements: [
+        ...elementsNew,
+        {
+          ...element,
+          height: elem.offsetHeight,
+        }
+      ]
+    });
+  }
+
+
+  resizeMouseDown = (e, dir, elementId) => {
+
     
+    e.preventDefault();
+    e.stopPropagation(); 
+
+    const element = this.state.elements.find(e => e.elementId === elementId);
+
+    const prevWidth = element.width;
+    const prevHeight = element.height;
+    const prevLeft = element.posX;
+    const prevTop = element.posY;
+
+    const mouseStartX = e.clientX;
+    const mouseStartY = e.clientY;
+
+    const resizeMouseMouve = (e) => {
+        let newWidth = prevWidth;
+        let newHeight = prevHeight;
+        let newLeft = prevLeft;
+        let newTop = prevTop;
+
+        const diffX = e.clientX - mouseStartX;
+        const diffY = e.clientY - mouseStartY;
+
+        switch(dir) {
+            case 'E': 
+                newWidth += diffX;
+                break;
+            case 'S': 
+                newHeight += diffY; 
+                break; 
+            case 'W': 
+                newWidth -= diffX;
+                newLeft += diffX;
+                break;    
+            case 'N': 
+                newHeight -= diffY;
+                newTop += diffY;
+                break;                 
+            case 'SE':
+                newWidth += diffX; 
+                newHeight += diffY; 
+                break; 
+            case 'NE':
+                newWidth += diffX; 
+                newHeight -= diffY; 
+                newTop += diffY; 
+                break; 
+            case 'SW':
+                newWidth -= diffX; 
+                newHeight += diffY; 
+                newLeft += diffX; 
+                break;
+            case 'NW':
+                newWidth -= diffX; 
+                newHeight -= diffY; 
+                newLeft += diffX; 
+                newTop += diffY; 
+                break; 
+            default: console.log('Invalid dir', dir)
+        }
+        const elem = document.getElementById(`${elementId}-input`);
+
+        let elementsNew = this.state.elements.filter(e => e.elementId !== elementId);
+        
+        this.setState({
+          elements: [
+            ...elementsNew,
+            {
+              ...element,
+                width: newWidth, 
+                height: element.type==='text' ? elem.offsetHeight : newHeight,
+                posX: newLeft,
+                posY: newTop,
+            }
+          ]
+        });
+    }
+    const mouseup = (e) => {
+        e.preventDefault();
+        e.stopPropagation(); 
+        console.log('removing Listener: ', dir);
+        window.removeEventListener('mousemove', resizeMouseMouve);
+        window.removeEventListener('mouseup', mouseup);
+    }
+    console.log('adding   Listener: ', dir);
+
+    window.addEventListener('mousemove', resizeMouseMouve);
+    window.addEventListener('mouseup', mouseup);
+}
+  
+  ////////////////////////////////////////////////////
+
+  getElements = () => {
+    return this.state.elements.map(e => {
+      switch(e.type){
+        case 'text': 
+          return(
+            <Rectangle 
+              currentlyEditing={this.state.editingId === e.elementId}
+              type='text'
+              key={e.elementId}
+              element={e}
+              elementId={e.elementId} 
+              selected={e.elementId === this.state.selectedId}
+              select={this.select}
+              selectAndEdit={this.selectAndEdit}
+              rectangleMouseDown={this.rectangleMouseDown}
+              onTextInput={this.onTextInput}
+              adjustTextElementHeight={this.adjustTextElementHeight}
+              resizeMouseDown={this.resizeMouseDown}
+            />
+          ) 
+        default: console.log('Invalid Element Type!'); 
+      }
+    })
   }
 
   render() {
@@ -49,22 +268,7 @@ class Creator extends Component {
       <div 
         className={styleClasses.join(" ")}
         onClick={this.unSelect}>
-        <Rectangle 
-          currentlyEditing={this.state.editingId === '5'}
-          type='text'
-          elementId={'5'} 
-          selected={'5' === this.state.selectedId}
-          select={this.select}
-          selectAndEdit={this.selectAndEdit}
-          />
-        <Rectangle 
-          currentlyEditing={this.state.editingId === '6'}
-          type='text'
-          elementId={'6'} 
-          selected={'6' === this.state.selectedId}
-          select={this.select}
-          selectAndEdit={this.selectAndEdit}
-          />
+        { this.getElements() }
       </div>
     );
   }
--- src/components/Creator/Rectangle/Rectangle.js
+++ src/components/Creator/Rectangle/Rectangle.js
@@ -6,40 +6,18 @@ import * as classes from './Rectangle.module.css';
 
 class Rectangle extends Component {
 
-    state = {
-        selected: false,
-        height: 200,
-        width: 200, 
-        posX: 200,
-        posY: 200,
-
-        text: 'oergnoiwrfgew',
-    }
-
     componentDidMount = () => {
         if(this.props.type === 'text'){
-            const elem = document.getElementById(`${this.props.elementId}-input`);
-            elem.innerHTML = this.state.text;
-            this.setState({height: elem.offsetHeight})
+            this.props.adjustTextElementHeight(this.props.elementId);
+            if(this.props.currentlyEditing){
+                this.onTextFocus(); 
+            }
         }
     }
 
-    componentDidUpdate = () => {
-        if(this.props.type === 'text' && this.props.currentlyEditing){
-            const elem = document.getElementById(`${this.props.elementId}-input`);
-            elem.focus(); 
-        }
-    }
-
-    onTextInput = (e) => {
-        console.log("onTextInput");
-        this.setState({text: e.target.innerHTML});
-        const elem = document.getElementById(`${this.props.elementId}-input`);
-        this.setState({height: elem.offsetHeight})
-    }
 
     onTextFocus = () => {
-        console.log('onTextFocus'); 
+        console.log('TEXT FOCUS END')
         const el = document.getElementById(`${this.props.elementId}-input`);
         if (typeof window.getSelection != "undefined"
             && typeof document.createRange != "undefined") {
@@ -57,136 +35,29 @@ class Rectangle extends Component {
         }
     }
 
-    onTextClick = (e) => {
-        e.stopPropagation();
-    }
-
-    rectangleMouseDown = e => {
-        console.log('Rectangle mousedown'); 
-        e.preventDefault();
-        e.stopPropagation();
-        this.props.select(e, this.props.elementId);
-        const prevX = this.state.posX - e.clientX;
-        const prevY = this.state.posY - e.clientY;
-        const mousemove = (e) => {
-            console.log('Rectangle mousemove'); 
-            const newX = prevX + e.clientX;
-            const newY = prevY + e.clientY;
-            this.setState({posX: newX, posY: newY})
-        }
-        const mouseup = (e) => {
-            console.log('Rectangle mouseup'); 
-            e.preventDefault();
-            e.stopPropagation();
-            window.removeEventListener('mousemove', mousemove);
-            window.removeEventListener('mouseup', mouseup);
-        }
-        window.addEventListener('mousemove', mousemove);
-        window.addEventListener('mouseup', mouseup);
-    }
-
-    
-    resizeMouseDown = (e, dir) => {
-        e.preventDefault();
-        e.stopPropagation(); 
-        const prevWidth = this.state.width;
-        const prevHeight = this.state.height;
-        const prevLeft = this.state.posX;
-        const prevTop = this.state.posY;
-
-        const mouseStartX = e.clientX;
-        const mouseStartY = e.clientY;
-
-        const resizeMouseMouve = (e) => {
-            let newWidth = prevWidth;
-            let newHeight = prevHeight;
-            let newLeft = prevLeft;
-            let newTop = prevTop;
-
-            const diffX = e.clientX - mouseStartX;
-            const diffY = e.clientY - mouseStartY;
-
-            switch(dir) {
-                case 'E': 
-                    newWidth += diffX;
-                    break;
-                case 'S': 
-                    newHeight += diffY; 
-                    break; 
-                case 'W': 
-                    newWidth -= diffX;
-                    newLeft += diffX;
-                    break;    
-                case 'N': 
-                    newHeight -= diffY;
-                    newTop += diffY;
-                    break;                 
-                case 'SE':
-                    newWidth += diffX; 
-                    newHeight += diffY; 
-                    break; 
-                case 'NE':
-                    newWidth += diffX; 
-                    newHeight -= diffY; 
-                    newTop += diffY; 
-                    break; 
-                case 'SW':
-                    newWidth -= diffX; 
-                    newHeight += diffY; 
-                    newLeft += diffX; 
-                    break;
-                case 'NW':
-                    newWidth -= diffX; 
-                    newHeight -= diffY; 
-                    newLeft += diffX; 
-                    newTop += diffY; 
-                    break; 
-                default: console.log('Invalid dir', dir)
-            }
-            const elem = document.getElementById(`${this.props.elementId}-input`);
-            this.setState({
-                width: newWidth, 
-                height: this.props.type==='text' ? elem.offsetHeight : newHeight,
-                posX: newLeft,
-                posY: newTop,
-            })
-        }
-        const mouseup = (e) => {
-            e.preventDefault();
-            e.stopPropagation(); 
-            console.log('removing Listener: ', dir);
-            window.removeEventListener('mousemove', resizeMouseMouve);
-            window.removeEventListener('mouseup', mouseup);
-        }
-        console.log('adding   Listener: ', dir);
-
-        window.addEventListener('mousemove', resizeMouseMouve);
-        window.addEventListener('mouseup', mouseup);
-    }
-
     getResizeHandlers = () => {
         if(!this.props.selected) return null;
         let resizeHandlers = [
-            <div key={`${this.props.elementId}-NW`} className={`${classes.Corner} ${classes.NW}`} onMouseDown={(e) => this.resizeMouseDown(e, 'NW')}></div>,
-            <div key={`${this.props.elementId}-SW`} className={`${classes.Corner} ${classes.SW}`} onMouseDown={(e) => this.resizeMouseDown(e, 'SW')}></div>,
-            <div key={`${this.props.elementId}-NE`} className={`${classes.Corner} ${classes.NE}`} onMouseDown={(e) => this.resizeMouseDown(e, 'NE')}></div>,
-            <div key={`${this.props.elementId}-SE`} className={`${classes.Corner} ${classes.SE}`} onMouseDown={(e) => this.resizeMouseDown(e, 'SE')}></div>,
-            <div key={`${this.props.elementId}-E`} className={`${classes.Edge} ${classes.E}`} onMouseDown={(e) => this.resizeMouseDown(e, 'E')}></div>,
-            <div key={`${this.props.elementId}-W`} className={`${classes.Edge} ${classes.W}`} onMouseDown={(e) => this.resizeMouseDown(e, 'W')}></div>,
+            <div key={`${this.props.elementId}-NW`} className={`${classes.Corner} ${classes.NW}`} onMouseDown={(e) => this.props.resizeMouseDown(e, 'NW', this.props.elementId)}></div>,
+            <div key={`${this.props.elementId}-SW`} className={`${classes.Corner} ${classes.SW}`} onMouseDown={(e) => this.props.resizeMouseDown(e, 'SW', this.props.elementId)}></div>,
+            <div key={`${this.props.elementId}-NE`} className={`${classes.Corner} ${classes.NE}`} onMouseDown={(e) => this.props.resizeMouseDown(e, 'NE', this.props.elementId)}></div>,
+            <div key={`${this.props.elementId}-SE`} className={`${classes.Corner} ${classes.SE}`} onMouseDown={(e) => this.props.resizeMouseDown(e, 'SE', this.props.elementId)}></div>,
+            <div key={`${this.props.elementId}-E`} className={`${classes.Edge} ${classes.E}`} onMouseDown={(e) => this.props.resizeMouseDown(e, 'E', this.props.elementId)}></div>,
+            <div key={`${this.props.elementId}-W`} className={`${classes.Edge} ${classes.W}`} onMouseDown={(e) => this.props.resizeMouseDown(e, 'W', this.props.elementId)}></div>,
         ]
         if(this.props.type !== 'text'){
-            resizeHandlers.push(<div key={`${this.props.elementId}-N`} className={`${classes.Edge} ${classes.N}`} onMouseDown={(e) => this.resizeMouseDown(e, 'N')}></div>);
-            resizeHandlers.push(<div key={`${this.props.elementId}-S`} className={`${classes.Edge} ${classes.S}`} onMouseDown={(e) => this.resizeMouseDown(e, 'S')}></div>);
+            resizeHandlers.push(<div key={`${this.props.elementId}-N`} className={`${classes.Edge} ${classes.N}`} onMouseDown={(e) => this.props.resizeMouseDown(e, 'N', this.props.elementId)}></div>);
+            resizeHandlers.push(<div key={`${this.props.elementId}-S`} className={`${classes.Edge} ${classes.S}`} onMouseDown={(e) => this.props.resizeMouseDown(e, 'S')}></div>);
         }
         return resizeHandlers
     }
 
     getStyles = () => {
         return {
-            height: `${this.state.height}px`, 
-            width: `${this.state.width}px`,
-            left: `${this.state.posX}px`,
-            top: `${this.state.posY}px`,
+            height: `${this.props.element.height}px`, 
+            width: `${this.props.element.width}px`,
+            left: `${this.props.element.posX}px`,
+            top: `${this.props.element.posY}px`,
         }
     }
 
@@ -194,14 +65,12 @@ class Rectangle extends Component {
         if(this.props.type !== 'text') return null;
         return(
             <p
-                contentEditable={this.props.currentlyEditing}
+                contentEditable={true}//this.props.currentlyEditing}
                 type='text' 
                 id={`${this.props.elementId}-input`}
                 className={classes.TextInput} 
-                onClick={this.onTextClick}
-                onDoubleClick={(e) => this.props.selectAndEdit(e, this.props.elementId)}
-                onFocus={this.onTextFocus}
-                onInput={this.onTextInput}>
+                onDoubleClick={this.onTextFocus}
+                onInput={e => this.props.onTextInput(e, this.props.elementId)}>
             </p>
         )
     }
@@ -211,8 +80,7 @@ class Rectangle extends Component {
         if(this.props.selected) styleClasses.push(classes.Selected);
         return(
                 <div className={styleClasses.join(' ')}
-                    onMouseDown={!this.props.currentlyEditing ? this.rectangleMouseDown : null}
-                    onClick={this.rectangleClick}
+                    onMouseDown={e => this.props.rectangleMouseDown(e, this.props.elementId)}
                     style={this.getStyles()}
                 >
                     {this.getTextInput()}
